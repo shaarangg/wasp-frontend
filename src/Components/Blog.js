78579ac2ca915ec05f69ab419da4b6005b147529
Shiv10 <shivanshsharma2012@gmail.com>
2021-11-14 21:18:02 +0530

    [WIP]: get blogs and display

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from'react'
+import React, { useEffect, useState } from'react'
 import Cookies from 'universal-cookie';
 import { useHistory } from 'react-router-dom';
 
@@ -7,15 +7,42 @@ const cookies = new Cookies();
 function Blog() {
 
     const history = useHistory();
+    const [blogs, setBlogs] = useState([]);
+
+    const getBlogs = async () => {
+
+        const requestOptions = {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        }; 
+
+        let res = await fetch('http://localhost:8080/blogs', requestOptions)
+        res = await res.json()
+        // res = JSON.parse(res)
+        console.log(res)
+        await setBlogs(res)
+        console.log('blogs')
+        console.log(blogs)
+    }
+
     useEffect (() => {
         const info = cookies.getAll();
         if(!info['username'] || !info['password']){
             history.push('/login')
         }
-    });
+        getBlogs();        
+    }, []);
     return (
         <div>
             <h1>Blogs</h1>
+            <hr/>
+            <br/>
+            {blogs.map( (blog) => {
+                <div id={blog.id}>
+                    <h3>{blog.title}</h3>
+                    <hr/>
+                </div>
+            } )}
         </div>
     )
 }
