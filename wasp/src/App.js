6dc8eea0c54a6ae6e085898ebcfe8ffb6e1a5fe3
Shiv10 <shivanshsharma2012@gmail.com>
2021-11-14 15:41:30 +0530

    feat: add links to nav

--- wasp/src/App.js
+++ wasp/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route} from "react-router-dom";
 import Login from './Components/Login';
 import Blog from './Components/Blog';
 import Nav from './Components/Nav';
--- wasp/src/Components/Nav.js
+++ wasp/src/Components/Nav.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Nav() {
     return (
         <nav>
             <h3>WASP</h3>
             <ul>
-                <li>Login</li>
-                <li>Blogs</li>
+                <Link to="/login">
+                    <li>Login</li>
+                </Link>
+                <Link to="/blogs">
+                    <li>Blogs</li>
+                </Link>
             </ul>
         </nav>
     )
