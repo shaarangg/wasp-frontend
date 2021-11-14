910c8e6c179b879153b604f1be0b9c75042c8a04
Shiv10 <shivanshsharma2012@gmail.com>
2021-11-14 15:08:58 +0530

    feat: init setup

---
+++ wasp/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from './Components/Login';
+import Blog from './Components/Blog';
+
+function App() {
+  return (
+    <div>
+      <h1>Have to setup router</h1>
+    </div>
+  );
+}
+
+export default App;
