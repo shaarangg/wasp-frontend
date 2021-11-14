77f9ee7df454bf4378c169b7eec9691e011c612f
Shaarang Singh <shaarangg.singh@gmail.com>
2021-11-14 21:41:07 +0530

    fix: Return

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,50 +1,64 @@
-import React, { useEffect, useState } from'react'
-import Cookies from 'universal-cookie';
-import { useHistory } from 'react-router-dom';
-
+import React, { useEffect, useState } from "react";
+import Cookies from "universal-cookie";
+import { useHistory } from "react-router-dom";
 
 const cookies = new Cookies();
 function Blog() {
+	const history = useHistory();
+	const [blogs, setBlogs] = useState([]);
 
-    const history = useHistory();
-    const [blogs, setBlogs] = useState([]);
+	const getBlogs = async () => {
+		const requestOptions = {
+			method: "GET",
+			headers: { "Content-Type": "application/json" },
+		};
 
-    const getBlogs = async () => {
+		let res = await fetch("http://localhost:8080/blogs", requestOptions);
+		res = await res.json();
+		setBlogs(res);
+	};
 
-        const requestOptions = {
-            method: 'GET',
-            headers: { 'Content-Type': 'application/json' },
-        }; 
-
-        let res = await fetch('http://localhost:8080/blogs', requestOptions)
-        res = await res.json()
-        // res = JSON.parse(res)
-        console.log(res)
-        await setBlogs(res)
-        console.log('blogs')
-        console.log(blogs)
-    }
-
-    useEffect (() => {
-        const info = cookies.getAll();
-        if(!info['username'] || !info['password']){
-            history.push('/login')
-        }
-        getBlogs();        
-    }, []);
-    return (
-        <div>
-            <h1>Blogs</h1>
-            <hr/>
-            <br/>
-            {blogs.map( (blog) => {
-                <div id={blog.id}>
-                    <h3>{blog.title}</h3>
-                    <hr/>
-                </div>
-            } )}
-        </div>
-    )
+	useEffect(() => {
+		const info = cookies.getAll();
+		if (!info["username"] || !info["password"]) {
+			history.push("/login");
+		}
+		getBlogs();
+	}, []);
+	if (blogs.length !== 0) {
+		console.log(blogs.length);
+		console.log(blogs);
+		return (
+			<div>
+				<h1>Blogs</h1>
+				<hr />
+				<br />
+				{blogs.map((blog) => {
+					return (
+						<div id={blog.id}>
+							<h3>{blog.title}</h3>
+							<hr />
+						</div>
+					);
+				})}
+			</div>
+		);
+	} else {
+		return <div>hello</div>;
+	}
+	return (
+		<div>
+			<h1>Blogs</h1>
+			<hr />
+			<br />
+			{blogs.map((blog) => {
+				<div id={blog.id}>
+					<h3>{blog.title}</h3>
+					<hr />
+				</div>;
+			})}
+		</div>
+	);
 }
 
-export default Blog
+export default Blog;
