891da8f946686cd1566b726b9c487971bd74fa4d
Shaarang Singh <shaarangg.singh@gmail.com>
2021-11-16 22:44:07 +0530

    feat: Styled the basic layout of the page

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -3,15 +3,17 @@ import { FaShareAlt } from "react-icons/fa";
 function Blog({ title, author, content }) {
 	return (
 		<article className="single-blog">
-			<div className="header">
+			<div className="blog-header">
+				<div className="blog-title">{title}</div>
+				<div className="blog-author">{author}</div>
+			</div>
+			<div className="blog-content">{content}</div>
+			<div className="blog-footer">
 				<div className="date">Posted Date</div>
 				<button className="share">
 					<FaShareAlt />
 				</button>
 			</div>
-			<div className="title">{title}</div>
-			<div className="content">{content}</div>
-			<div className="footer">{author}</div>
 		</article>
 	);
 }
